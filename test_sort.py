12e21928e2235adc04a5e677741227817c1c224f
Jorge Camacho <89266161+JorG96@users.noreply.github.com>
2025-03-13 15:13:29 -0400

    Update test_sort.py

--- test_sort.py
+++ test_sort.py
@@ -1,23 +1,24 @@
-import pytest
 from challenge import sort
+
 
 def test_standard_package():
     """Test packages that should be classified as STANDARD"""
 
     assert sort(12, 14, 10, 3) == "STANDARD"
-    
+
 
 def test_special_package_by_volume():
     """Test packages that should be SPECIAL due to volume"""
-    
+
     assert sort(100, 100, 100, 15) == "SPECIAL"
 
 
 def test_special_package_by_dimension():
     """Test packages that should be SPECIAL due to single dimension"""
-  
+
     assert sort(150, 10, 10, 15) == "SPECIAL"
     assert sort(160, 10, 10, 15) == "SPECIAL"
+
 
 def test_special_package_by_mass():
     """Test packages that should be SPECIAL due to mass"""
@@ -25,10 +26,10 @@ def test_special_package_by_mass():
     assert sort(10, 10, 10, 20) == "SPECIAL"
     assert sort(10, 10, 10, 25) == "SPECIAL"
 
+
 def test_rejected_package():
     """Test packages that should be REJECTED"""
 
     assert sort(100, 100, 100, 20) == "REJECTED"
     assert sort(150, 10, 10, 20) == "REJECTED"
     assert sort(200, 200, 200, 25) == "REJECTED"
-
